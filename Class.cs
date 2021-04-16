2c614920caa11d4264ea40fb282648434d8868c1
Juan Emilio Malacalza <73360894+JuanEmilioM@users.noreply.github.com>
2021-04-16 17:20:31 -0300

    Add files via upload

---
+++ Class.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace Monticulo_Binario
+{
+	public partial class MonticuloBinario <T> where T : IComparable <T>
+	{
+		T[] vector;
+		int tamActual;
+		bool ordenado;
+		
+		public MonticuloBinario (int n)
+		{
+			GetVector(n);
+			tamActual=0; // Tamaño actual
+			ordenado=true;
+			
+			Inicializar();
+		}
+
+		public MonticuloBinario() : this(20) { }
+
+		public bool Vacio()
+		{
+			return(tamActual==0);
+		}
+
+		private void Inicializar()
+		{
+			for(int i = 0; i < vector.Length; i++)
+				vector[i] = default(T);
+		}
+		
+		public void Limpiar ()
+		{
+			Inicializar();
+			tamActual = 0;
+			ordenado=true;
+		}
+		
+		public T Minimo ()
+		{
+			if(Vacio())
+				throw new Exception("Árbol vacio");
+			
+			if(!ordenado)
+				ArreglarMonticulo();
+			return vector[1];
+		}
+		
+		public int Introducir (T valor)
+		{
+			ComprobarTamanio();
+			vector[++tamActual]=valor;
+			
+			if(tamActual == 1)
+				ordenado=true;
+			else
+				if(valor.CompareTo(vector[tamActual/2]) < 0)
+					ordenado=false;
+			
+			return 0;
+		}
+		
+		public int Insertar (T valor)
+		{
+			if(Vacio()) {
+				vector[++tamActual]=valor;
+				return 0;
+			}
+			
+			if(!ordenado)
+				return Introducir(valor);
+			
+			ComprobarTamanio();
+			
+			// Reflotamiento
+			int hueco= ++tamActual;
+			while(valor.CompareTo(vector[hueco/2])<0) {
+				vector[hueco]=vector[hueco/2];
+				hueco/=2;
+				if(vector[hueco/2]==null)
+					break;
+			}
+			
+			vector[hueco]=valor;
+			
+			return 0;
+		}
+		
+		public T EliminarMinimo ()
+		{
+			T minimo=Minimo();
+			vector[1]=vector[tamActual--];
+			Hundir(1);
+			
+			return minimo;
+		}
+		
+	}
+}
---
+++ Implementación interna.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Monticulo_Binario
+{
+	public partial class MonticuloBinario <T>
+	{
+		private void GetVector (int tamanio)
+		{
+			vector=new T[tamanio];
+		}
+		
+		private void ComprobarTamanio ()
+		{
+			if(tamActual >= vector.Length) {
+				T[] auxVector=vector;
+				GetVector(tamActual*2);
+				for(int i=0; i<auxVector.Length; i++)
+					vector[i]=auxVector[i];
+			}
+		}
+		
+		private void Hundir (int hueco)
+		{
+			int hijo;
+			T temp=vector[hueco];
+			
+			for(; hueco*2 <= tamActual; hueco=hijo) {
+				hijo=hueco*2;
+				if((hijo != tamActual) && (vector[hijo+1].CompareTo(vector[hijo]) < 0))
+					hijo++;
+				
+				if(vector[hijo].CompareTo(temp) < 0)
+					vector[hueco]=vector[hijo];
+				else
+					break;
+			}
+				
+			vector[hueco]=temp;
+		}
+		
+		private void ArreglarMonticulo ()
+		{
+			for(int i=tamActual/2; i>0; i--)
+				Hundir(i);
+			ordenado=true;
+		}
+	}
+}
