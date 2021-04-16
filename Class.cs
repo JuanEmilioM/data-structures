bf0bedb0ccf16d73118048b53561b34b303b39d1
Juan Emilio Malacalza <73360894+JuanEmilioM@users.noreply.github.com>
2021-04-16 16:32:23 -0300

    Add files via upload

--- Class.cs
+++ Class.cs
@@ -1,257 +1,154 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
-namespace Lista_Encadenada
+namespace Lista_Secuencial
 {
-	class Nodo <tipoItem> 
-	{
-		int indice;
-		tipoItem dato;
-		Nodo<tipoItem> sig;
-		
-		public Nodo ()
-		{
-			dato=default(tipoItem);
-			sig=null;
-		}
-		
-		public Nodo <tipoItem> getSiguiente ()
-		{
-			return sig;
-		}
-		
-		public void setSiguiente (Nodo<tipoItem> dir)
-		{
-			sig=dir;
-		}
-		
-		public void setSiguiente ()
-		{
-			sig=null;
-		}
-		
-		public tipoItem getDato ()
-		{
-			return dato;
-		}
-		
-		public void setDato (tipoItem x)
-		{
-			dato=x;
-		}
-		
-		public void setIndice (int i)
-		{
-			indice=i;
-		}
-	
-		public int getIndice ()
-		{
-			return indice;
-		}
-	}
-	
-	public class Lista <tipoItem> where tipoItem : IComparable <tipoItem>
-	{
-		Nodo<tipoItem> cabeza;
-		int nodos;
-		
-		public Lista ()
-		{
-			cabeza=null;
-			nodos=0;
-		}
-		
-		public bool Vacia ()
-		{
-			return(cabeza==null);
-		}
-				
-		public void Insertar (tipoItem dato)
-		{
-				Nodo<tipoItem> nuevo=new Nodo<tipoItem>();
-				nuevo.setIndice(1);
-				nuevo.setDato(dato);
-				
-				if(Vacia())
-					nuevo.setSiguiente();
-				else
-					nuevo.setSiguiente(cabeza);
+	public class Lista <tipoItem> where tipoItem : IEquatable <tipoItem>
+    {
+        tipoItem[] items;
+        int maximo;
+        int ultimo;
+
+        public Lista(int n)
+        {
+            items = new tipoItem[n];
+            maximo = n;
+            ultimo = -1;
+
+            Inicializar();
+        }
+
+        public Lista() : this(20) { }
+
+        public bool Vacia()
+        {
+            return (ultimo == -1);
+        }
+
+        private void Inicializar()
+        {
+            for (int i = 0; i < maximo; i++)
+                items[i] = default(tipoItem);
+        }
+
+        private void OffsetR (int i)
+        {
+            for(int j=ultimo; j>=i; j--)
+                items[j+1]=items[j];
+        }
+
+        private void OffsetL (int i)
+        {
+            for(int j=i; j<ultimo; j++)
+                items[j]=items[j+1];
+
+            items[ultimo] = default(tipoItem);
+        }
+        
+        public tipoItem this [int indice]
+        {
+        	get
+        	{
+        		return Recuperar(indice);
+        	}
+        	
+        	set
+        	{
+        		Insertar(indice,value);
+        	}
+        }
+        
+        public void Insertar (tipoItem dato)
+        {
+        	Insertar(1,dato);
+        }
+
+        private void Insertar(int i, tipoItem dato)
+        {
+            i--;
 			
-				cabeza=nuevo;
-				nodos++;
-				
-				if(!Vacia())
-					IncrementarNodos(nuevo.getSiguiente());
-		}
-		
-		private void Insertar (int i, tipoItem dato)
-		{
-			if((i < 1) || (i > nodos+1))
-				throw new Exception("Posiciones válidas de 1 a " + (nodos+1));
-												
-			if(i==1) {
-				Insertar(dato);
-				return;
-			}
-			
-			// Buscar posición de inserción
-			int j=0;
-			Nodo<tipoItem> aux=cabeza;
-			Nodo<tipoItem> anterior=aux;;
-			
-			while((j < nodos) && (aux.getIndice() != i)) {
-				anterior=aux;
-				aux=aux.getSiguiente();
-				j++;
-			}
-			
-			Nodo<tipoItem> nuevo=new Nodo<tipoItem>();
-			nuevo.setIndice(i);
-			nuevo.setDato(dato);
-			nuevo.setSiguiente(aux);
-			anterior.setSiguiente(nuevo);
-			nodos++;
-			
-			IncrementarNodos(nuevo.getSiguiente());
-		}
-				
-		public void Agregar (tipoItem dato)
-		{
-			this[nodos+1]=dato;
-		}
-		
-		public tipoItem Suprimir (int i)
-		{
-			int j=0;
-			tipoItem x=default(tipoItem);
-			Nodo<tipoItem> aux=cabeza;
-			Nodo<tipoItem> anterior=aux;;
-			
-			if(Vacia())
-				throw new Exception("Lista vacía");
-			
-			if((i-1 < 0) || (i > nodos))
-				throw new Exception("Indice incorrecto");
-			
-			// Supresión del primer elemento de la Lista
-			if(i == 1) {
-				x=cabeza.getDato();
-				cabeza=cabeza.getSiguiente();
-				aux=null;
-				nodos--;
-				
-				DecrementarNodos(cabeza);
-				
-				return x;
-			}
-			
-			while((j < nodos) && (aux.getIndice() != i)) {
-				anterior=aux;
-				aux=aux.getSiguiente();
-				j++;
-			}
-			
-			anterior.setSiguiente(aux.getSiguiente());
-			x=aux.getDato();
-			aux=null;
-			nodos--;
-			
-			DecrementarNodos(anterior.getSiguiente());
-			
-			return x;
-		}
-		
-		public tipoItem this [int indice]
-		{
-			get
-			{
-				return Recuperar(indice);
-			}
-			
-			set
-			{
-				Insertar(indice,value);
-			}
-		}
-		
-		private tipoItem Recuperar (int i)
-		{
-			int j=0;
-			Nodo<tipoItem> aux=cabeza;
-			
-			if(Vacia())
-				throw new Exception("Lista vacía");
-			
-			if((i-1 < 0) || (i > nodos))
-				throw new Exception("Indice incorrecto");
-			
-			while((j < nodos) && (aux.getIndice() != i)) {
-				aux=aux.getSiguiente();
-				j++;
-			}
-			
-			return aux.getDato();
-		}
-		
-		
-		public tipoItem Primero ()
-		{
-			return this[1];
-		}
-		
-		public tipoItem Ultimo ()
-		{
-			return this[nodos];
-		}
-		
-		public int Buscar (tipoItem dato)
-		{
-			Nodo<tipoItem> aux=cabeza;
-			
-			while((aux != null) && (aux.getDato().CompareTo(dato)) != 0)
-				aux=aux.getSiguiente();
-			
-			if(aux!=null)
-				return aux.getIndice();
-			else
-				return -1;
-		}
-				
-		public void Limpiar ()
-		{	
-			Nodo<tipoItem> aux;
-			
-			while(cabeza != null) {
-				aux=cabeza;
-				aux=null;
-				cabeza=cabeza.getSiguiente();
-			}
-			
-			nodos=0;
-		}
-			
-		private void IncrementarNodos (Nodo<tipoItem> x)
-		{
-			while(x != null) {
-				x.setIndice(x.getIndice()+1);
-				x=x.getSiguiente();
-			}
-		}
-		
-		private void DecrementarNodos (Nodo<tipoItem> x)
-		{
-			while(x != null) {
-				x.setIndice(x.getIndice()-1);
-				x=x.getSiguiente();
-			}
-		}
-		
-		public int Nodos
-		{
-			get 
-			{
-				return nodos;
-			}
-		}
-	}
+            if(ultimo+1 >= maximo)
+            	throw new Exception("Espacio insuficiente");
+            
+            if ((i < 0) || (i > ultimo + 1))
+            	throw new Exception("Posiciones válidas de 1 a " + (ultimo+2));            
+                        
+            OffsetR(i);
+            items[i] = dato;
+            ultimo++;
+        }
+        
+        public void Agregar (tipoItem dato)
+        {
+        	this[ultimo+2]=dato;
+        }
+
+        public tipoItem Suprimir(int i)
+        {
+            i--;
+
+            tipoItem x;
+
+            if(Vacia()) 
+                throw new Exception("Lista vacía");
+                
+            if((i < 0) || (i > ultimo))
+            	throw new Exception("Indice incorrecto");
+
+            x = items[i];
+            OffsetL(i);
+            ultimo--;
+
+            return x;
+        }
+
+        private tipoItem Recuperar(int i)
+        {
+            i--;
+
+            if(Vacia()) 
+            	throw new Exception("Lista vacía");
+            
+            if((i<0) || (i>ultimo))
+            	throw new Exception("Indice incorrecto");
+            
+            return items[i];
+        }
+
+        public int Buscar(tipoItem dato)
+        {
+            int i = 0;
+            while ((i <= ultimo) && (!items[i].Equals(dato)))
+            	i++;
+
+            if (i > ultimo)
+                return -1;
+            else
+                return i+1;
+        }
+
+        public void Limpiar ()
+        {
+            Inicializar();
+            ultimo = -1;
+        }
+
+        public tipoItem Primero ()
+        {
+        	return this[1];
+        }
+
+        public tipoItem Ultimo()
+        {
+        	return this[ultimo+1];
+        }
+
+        public int Cantidad
+        {
+            get
+            {
+                return ultimo + 1;
+            }
+        }
+    }
 }
