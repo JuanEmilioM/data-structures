ef9087de5dc57dc2484fa801a087a6a6468c005f
Juan Emilio Malacalza <73360894+JuanEmilioM@users.noreply.github.com>
2021-04-16 00:25:53 -0300

    Add files via upload

---
+++ Class.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Cola_Encadenada
+{
+	class Celda <tipoItem>
+	{
+		tipoItem dato;
+		Celda<tipoItem> sig;
+		
+		public Celda ()
+		{
+			dato=default(tipoItem);
+			sig=null;
+		}
+		
+		public Celda <tipoItem> getSiguiente ()
+		{
+			return sig;
+		}
+		
+		public void setSiguiente (Celda<tipoItem> dir)
+		{
+			sig=dir;
+		}
+		
+		public void setSiguiente ()
+		{
+			sig=null;
+		}
+		
+		public tipoItem getDato ()
+		{
+			return dato;
+		}
+		
+		public void setDato (tipoItem x)
+		{
+			dato=x;
+		}
+	}
+	
+	public class Cola <tipoItem>
+	{
+		Celda<tipoItem> primero;
+		Celda<tipoItem> ultimo;
+		
+		public Cola ()
+		{
+			primero=null;
+			ultimo=null;
+		}
+		
+		public bool Vacia ()
+		{
+			return (primero == null);
+		}
+		
+		public int Insertar (tipoItem x)
+		{
+			Celda<tipoItem> aux = new Celda<tipoItem>();
+			
+			aux.setDato(x);
+			aux.setSiguiente();
+			
+			if(ultimo == null) {
+				primero=aux;
+				ultimo=aux;
+			}
+			else {
+				ultimo.setSiguiente(aux);
+				ultimo=aux;
+			}
+			
+			return 0;
+		}
+		
+		public tipoItem Quitar ()
+		{
+			if(Vacia())
+				throw new Exception("Cola vacía");
+			
+			Celda<tipoItem> aux=primero;
+			tipoItem x = aux.getDato();
+			primero= aux.getSiguiente();
+			
+			aux=null;
+			
+			if(primero == null)
+				ultimo=null;
+				
+			return x;
+		}
+		
+		public tipoItem Primero ()
+		{
+			if(Vacia())
+				throw new Exception("Cola vacía");
+			
+			return primero.getDato();
+		}
+	}
+}
---
+++ Cola Encadenada.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace ColaEncadenada
+{
+    class Celda<tipoItem>
+    {
+        tipoItem item;
+        Celda<tipoItem> sig;
+
+        public Celda()
+        {
+            sig = null;
+        }
+
+        public tipoItem getItem()
+        {
+            return item;
+        }
+
+        public void setItem(tipoItem x)
+        {
+            item = x;
+        }
+
+        public void setSig(Celda<tipoItem> dir)
+        {
+            sig = dir;
+        }
+
+        public Celda<tipoItem> getSig()
+        {
+            return sig;
+        }
+
+        ~Celda() { }
+    }
+
+    public class Cola<tipoItem>
+    {
+        int cant;
+
+        Celda<tipoItem> primero;
+        Celda<tipoItem> ultimo;
+
+        public Cola()
+        {
+            primero = null;
+            ultimo = null;
+            cant = 0;
+        }
+
+        public bool Vacia()
+        {
+            return (cant == 0);
+        }
+
+        public void Insertar(tipoItem x)
+        {
+            Celda<tipoItem> aux = new Celda<tipoItem>();
+            aux.setItem(x);
+            aux.setSig(null);
+
+            if (ultimo == null)
+            {
+                primero = aux;
+                cant++;
+            }
+            else
+            {
+                ultimo.setSig(aux);
+                ultimo = aux;
+                cant++;
+            }
+        }
+        
+        private tipoItem Suprimir()
+        {
+            Celda<tipoItem> aux;
+            tipoItem x;
+
+            if (Vacia())
+                return default(tipoItem);
+
+            x = primero.getItem();
+            aux = primero;
+            primero = primero.getSig();
+            cant--;
+
+            aux = null;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            
+            return x;
+        }
+
+
+        public int Cantidad
+        {
+            get
+            {
+                return cant;
+            }
+        }
+
+        // Devuelve el primer elemento de la Cola y lo elimina
+        public tipoItem Primero
+        {
+            get
+            {
+                return Suprimir();
+            }
+        }
+    }
+}
